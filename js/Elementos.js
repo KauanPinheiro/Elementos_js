d4fca74504cd067453d9585dfb126579c77c0219
KAUAN PINHEIRO DO NASCIMENTO <kauan.pnascimento@senacsp.edu.br>
2023-09-20 11:56:12 -0300

    elementos

---
+++ js/Elementos.js
@@ -0,0 +1,23 @@
+/*
+//pegando um elemeento pelo id 
+let viagem = document.getElementById("sirius");
+
+viagem.value = "Senac Largo Treze";
+
+console.log(viagem.value);
+
+
+//pegando um elemento pelo name
+
+let moeda = document.getElementsByName("opt-moedas");
+console.log(moeda[0].value);
+console.log(moeda[1].value);
+console.log(moeda[2].value);
+//pegando um elemento pela tag
+*/
+let viajar = document.getElementsByTagName("span");
+
+viajar.value = "Estou viajando";
+
+console.log(viajar.value);
+
